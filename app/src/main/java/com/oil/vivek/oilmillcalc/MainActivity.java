944234d07120cf549360062f4de71d86d266d4a0
vivek.vikani <vivek.vikani@rakuten.com>
2017-02-07 10:02:06 +0900

    Mac | Groundnut Seed added | Permissions check Marshmallow | Version 2.3

--- app/src/main/java/com/oil/vivek/oilmillcalc/MainActivity.java
+++ app/src/main/java/com/oil/vivek/oilmillcalc/MainActivity.java
@@ -2,16 +2,12 @@ package com.oil.vivek.oilmillcalc;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
